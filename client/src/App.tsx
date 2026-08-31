c201abaf56753d89062117cc880cf22b198d4dc0
walawala254 <dbmbryson@gmail.com>
2026-08-31 14:44:41 +0300

    Render portfolio directly on GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,8 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-
-
-function SiteRoutes() {
-  return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 // NOTE: About Theme
 // - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
@@ -32,9 +18,7 @@ function App() {
       >
         <TooltipProvider>
           <Toaster />
-          <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "") || "/"}>
-            <SiteRoutes />
-          </WouterRouter>
+          <Home />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
